35c4decda51cfed58a30fe4a1a2e5018c5d14e5f
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-09 19:15:04 +0100

    json server

--- src/compontents/streams/StreamCreate.js
+++ src/compontents/streams/StreamCreate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import { connect } from 'react-redux';
+import { createStream } from '../../actions'
 
 class StreamCreate extends React.Component {
     renderError({error, touched }){
@@ -24,8 +25,8 @@ class StreamCreate extends React.Component {
         );
     }
 
-    onSubmit(formValues){
-        console.log(formValues)
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
     }
 
     render(){
@@ -47,10 +48,13 @@ const validate = formValues => {
     if(!formValues.description){
         errors.description = 'You must enter a description'
     }
+
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
