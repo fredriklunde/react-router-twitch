d38326ed0488625fbab6be6aec3ddfadc27a8063
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-10 08:30:41 +0100

    streamReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,14 +19,14 @@ export const signIn = (userId) => {
 export const signOut = () => {
     return {
         type: SIGN_OUT
-    };
-};
+    }
+}
 
 export const createStream = formValues => async dispatch => {
     const response =  await streams.post('/streams', formValues);
 
     dispatch({ type: CREATE_STREAM, payload: response.data }) 
-};
+}
 
 export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
