db2371d67cfca6edf257771e5a910f835f191b01
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-12-27 12:40:02 +0100

    dismiss reusable

--- src/compontents/streams/StreamDelete.js
+++ src/compontents/streams/StreamDelete.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
     const actions = (
         <React.Fragment>
             <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
+            <button onClick={() => history.push('/')}className="ui button">Cancel</button>
         </React.Fragment>
     );
 
@@ -16,6 +17,7 @@ const StreamDelete = () => {
                 title="Delete Stream"
                 content="Are you sure you want to delete this stream?"
                 actions={actions}
+                dismiss={()=> history.push('/')}
             />
         </div>
         
